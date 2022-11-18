22f3ef2cf9af671e19e4c7965baa5ec9bbf2d206
A Daraseliya <avdaraseliya@sci.pfu.edu.ru>
2022-11-18 12:07:05 +0300

    Task 2.3 Update 1 Копия кода из doc файла выше

--- Task_2_3/task_2_3_input.sql
+++ Task_2_3/task_2_3_input.sql
@@ -1,89 +1,221 @@
+/*
+ 1. Создать таблицу с основной информацией о сотрудниках: ФИО, дата рождения, дата начала работы, должность, уровень сотрудника (jun, middle, senior, lead), уровень зарплаты, идентификатор отдела, наличие/отсутствие прав(True/False). При этом в таблице обязательно должен быть уникальный номер для каждого сотрудника. 
+4. Несколько уточнений по предыдущим заданиям – в первой таблице должны быть записи как минимум о 5 сотрудниках, которые работают как минимум в 2-х разных отделах. Содержимое соответствующих атрибутов остается на совесть вашей фантазии, но, желательно соблюдать осмысленность и правильно выбирать типы данных (для зарплаты – числовой тип, для ФИО – строковый и т.д.) 
+*/
+
+-- DROP TABLE employees;
+
 CREATE TABLE employees
 (
 	Id SERIAL PRIMARY KEY,
-	LastName CHARACTER VARYING(20) NOT NULL,
-	FirstName CHARACTER VARYING(20) NOT NULL,
-	MiddleName CHARACTER VARYING(20),
+	FullName CHARACTER VARYING(80) NOT NULL,
 	BirthDate date,
 	StartDate date,
 	JobTitle CHARACTER VARYING(30),
 	EmployeeLevel CHARACTER VARYING(20),
 	Salary INTEGER,
 	DepartmentID INTEGER,
-	RightsPresenceAbsence BOOLEAN
+	DriverLicense BOOLEAN
 );
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Иванов','Иван','Иванович','1970-1-1','2012-9-12','Инженер','senior',150000,1,'True');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Иванов Иван Иванович', '1970-1-1','2012-9-12','Инженер','senior',150000,1,'True');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Перов','Константин','Игоревич','1971-1-1','2010-9-22','Инженер','lead',155000,1,'True');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Перов Константин Игоревич', '1971-1-1','2010-9-22','Инженер','lead',155000,1,'True');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Яковлев','Максим','Игоревич','1991-1-1','2020-5-22','Ассистент','jun',45000,1,'True');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Яковлев Максим Игоревич', '1991-1-1','2020-5-22','Ассистент','jun',45000,1,'True');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Чесноков','Игорь','Игоревич','1994-3-1','2021-6-22','Лаборант','jun',40000,2,'False');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Чесноков Игорь Игоревич', '1994-3-1','2021-6-22','Лаборант','jun',40000,2,'False');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Числов','Игорь','Игоревич','1992-3-1','2022-6-12','Лаборант','jun',40000,3,'False');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Числов Игорь Игоревич','1992-3-1','2022-6-12','Лаборант','jun',40000,3,'False');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Кротов','Игорь','Игоревич','1992-3-1','2022-6-12','Лаборант','jun',40000,3,'True');
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Кротов Игорь Игоревич','1992-3-1','2022-6-12','Лаборант','jun',40000,3,'True');
+
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Сидоров Семен Игоревич' ,'1992-3-1','2020-6-12','Инженер','lead',65000,3,'True');
+
+-- SELECT * FROM employees;
+
+/* 
+2. Для будущих отчетов аналитики попросили вас создать еще одну таблицу с информацией по отделам – в таблице должен быть идентификатор для каждого отдела, название отдела (например. Бухгалтерский или IT отдел), ФИО руководителя и количество сотрудников.
+*/
 
 CREATE TABLE departments
 (
 	Id SERIAL PRIMARY KEY,
 	DepartmenrTitle CHARACTER VARYING(60) NOT NULL,
-	LeaderLastName CHARACTER VARYING(20),
-	LeaderFirstName CHARACTER VARYING(20),
-	LeaderMiddleName CHARACTER VARYING(20),
+	LeaderFullName CHARACTER VARYING(80) NOT NULL,
 	NumberOfEmployees INTEGER
 );
 
-INSERT INTO departments(DepartmenrTitle,LeaderLastName,NumberOfEmployees)
-VALUES ('Кафедра ИТ', 'Семенов', 20);
+INSERT INTO departments(DepartmenrTitle,LeaderFullName,NumberOfEmployees)
+VALUES ('Кафедра ИТ', 'Семенов Василий Иванович', 20);
 
-INSERT INTO departments(DepartmenrTitle,LeaderLastName,NumberOfEmployees)
-VALUES ('Институт ПМ', 'Кадров', 15);
+INSERT INTO departments(DepartmenrTitle,LeaderFullName,NumberOfEmployees)
+VALUES ('Институт ПМ', 'Кадров Виктор Викторович', 15);
 
-INSERT INTO departments(DepartmenrTitle,LeaderLastName,NumberOfEmployees)
-VALUES ('Кафедра ПХ', 'Орлов', 19);
+INSERT INTO departments(DepartmenrTitle,LeaderFullName,NumberOfEmployees)
+VALUES ('Кафедра ПХ', 'Орлов Александр Валерьевич', 19);
+
+-- SELECT * FROM departments;
+
+/*        
+3. На кону конец года и необходимо выплачивать сотрудникам премию. Премия будет выплачиваться по совокупным оценкам, которые сотрудники получают в каждом квартале года. Создайте таблицу, в которой для каждого сотрудника будут его оценки за каждый квартал. Диапазон оценок от A – самая высокая, до E – самая низкая.
+*/
 
 CREATE TABLE estimates
 (
-	LastName CHARACTER VARYING(20),
-	FirstName CHARACTER VARYING(20),
-	MiddleName CHARACTER VARYING(20),
+	EmplId INTEGER,
 	Q1estimate CHARACTER VARYING(2),
 	Q2estimate CHARACTER VARYING(2),
 	Q3estimate CHARACTER VARYING(2),
 	Q4estimate CHARACTER VARYING(2)
 );
 
-INSERT INTO estimates(LastName,FirstName,MiddleName,Q1estimate,Q2estimate,Q3estimate,Q4estimate)
-VALUES ('Иванов','Иван','Иванович', 'A','A','A','A');
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (1, 'A','A','A','A');
 
-INSERT INTO estimates(LastName,FirstName,MiddleName,Q1estimate,Q2estimate,Q3estimate,Q4estimate)
-VALUES ('Перов','Константин','Игоревич','A','B','A','A');
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (2, 'A','B','A','A');
 
-INSERT INTO estimates(LastName,FirstName,MiddleName,Q1estimate,Q2estimate,Q3estimate,Q4estimate)
-VALUES ('Числов','Игорь','Игоревич','E','B','D','A');
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (3, 'C','B','D','A');
+
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (4, 'B','B','B','A');
+
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (5, 'E','B','D','A');
+
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (6 ,'D','B','D','A');
+
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (7,'D','B','D','A');
+
+-- SELECT * FROM estimates;
+
+/* 
+5. Ваша команда расширяется и руководство запланировало открыть новый отдел – отдел Интеллектуального анализа данных. На начальном этапе в команду наняли одного руководителя отдела и двух сотрудников. Добавьте необходимую информацию в соответствующие таблицы.
+*/
+
+INSERT INTO departments(DepartmenrTitle,LeaderFullName,NumberOfEmployees)
+VALUES ('отдел Интеллектуального анализа данных', 'Орехов Иван Алексеевич', 3);
+
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Орехов Иван Алексеевич','1972-1-1','2012-9-12','Руководитель отдела','lead',300000,4,'True');
+
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Степанов Семен Семенович','1970-1-1','2012-9-12','Дата Инженер','senior',150000,4,'True');
+
+INSERT INTO employees(FullName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,DriverLicense)
+VALUES ('Орлов Александр Валерьевич', '1970-1-1','2012-9-12','Инженер','jun',100000,4,'True');
+
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (8,'A','B','B','A');
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (9,'A','B','B','A');
+INSERT INTO estimates(EmplId, Q1estimate,Q2estimate,Q3estimate,Q4estimate)
+VALUES (10,'A','B','A','A');
+
+/*
+6. Теперь пришла пора анализировать наши данные – напишите запросы для получения следующей информации:
+*/
+/*
+   Уникальный номер сотрудника, его ФИО и стаж работы – для всех сотрудников компании
+*/
+
+SELECT Id,FullName,StartDate,'2022-11-04'-StartDate as WorkExperienceDays
+FROM employees;
 
 
-INSERT INTO estimates(LastName,FirstName,MiddleName,Q1estimate,Q2estimate,Q3estimate,Q4estimate)
-VALUES ('Кротов','Игорь','Игоревич','D','B','D','A');
+/*
+ Уникальный номер сотрудника, его ФИО и стаж работы – только первых 3-х сотрудников
+*/
+
+SELECT Id,FullName,'2022-11-04'-StartDate as WorkExperienceDays
+FROM employees
+WHERE Id<4;
 
 
-INSERT INTO departments(DepartmenrTitle,LeaderLastName,NumberOfEmployees)
-VALUES ('отдел Интеллектуального анализа данных', 'Орехов', 3);
+/*
+второй вариант:
+*/
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Орехов','Иван','Алексеевич','1972-1-1','2012-9-12','Руководитель отдела','lead',300000,4,'True');
+SELECT Id,FullName,'2022-11-04'-StartDate as WorkExperienceDays
+FROM employees
+LIMIT 3;
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Степанов','Семен','Семенович','1970-1-1','2012-9-12','Дата Инженер','senior',150000,4,'True');
 
-INSERT INTO employees(LastName,FirstName,MiddleName,BirthDate,StartDate,JobTitle,EmployeeLevel,Salary,DepartmentID,RightsPresenceAbsence)
-VALUES ('Орлов','Александр','Валерьевич','1970-1-1','2012-9-12','Инженер','jun',100000,4,'True');
+
+  /*   
+Уникальный номер сотрудников - водителей
+*/
+SELECT Id
+FROM employees
+WHERE DriverLicense = 'True';
+
+
+/*
+проверка
+*/
+
+SELECT Id,FullName,DriverLicense
+FROM employees
+WHERE DriverLicense = 'True';
+
+/*
+Выведите номера сотрудников, которые хотя бы за 1 квартал получили оценку D или E
+*/
+
+SELECT EmplId
+FROM estimates
+WHERE Q1estimate = 'D' OR Q1estimate = 'E' OR Q2estimate = 'D' OR Q2estimate = 'E' OR Q3estimate = 'D' OR Q3estimate = 'E'OR Q4estimate = 'D' OR Q4estimate = 'E';
+
+
+/*
+Выведите самую высокую зарплату в компании.
+*/
+
+SELECT MAX(Salary)
+FROM employees;
+
+
+/*
+ Выведите название самого крупного отдела
+*/
+SELECT DepartmenrTitle 
+FROM departments
+WHERE NumberOfEmployees=(SELECT MAX(NumberOfEmployees) FROM departments);
+
+
+/*
+ Выведите номера сотрудников от самых опытных до вновь прибывших
+*/
+SELECT Id, '2022-11-04'-StartDate as WorkExperienceDays
+FROM employees
+ORDER BY StartDate ASC;
+
+
+/*
+ Рассчитайте среднюю зарплату для каждого уровня сотрудников
+*/
+SELECT AVG(Salary) AS SalaryAvg
+FROM employees
+WHERE EmployeeLevel='jun';
+
+
+/*
+для всех
+*/ 
+
+SELECT EmployeeLevel, AVG(Salary) AS SalaryAvg
+FROM employees
+GROUP BY EmployeeLevel;
+
 
